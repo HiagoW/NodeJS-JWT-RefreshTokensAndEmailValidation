5aaeec14c7bb3c6d5d614fbdf3d23d43297092b9
Hiago Petris <hiago.petris@gmail.com>
2022-06-27 08:41:15 -0300

    Email verification

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -3,9 +3,9 @@ const { InvalidArgumentError } = require('../erros');
 const tokens = require('./tokens')
 const {EmailVerificacao} = require('./emails');
 
-function geraEndereco(rota, id) {
+function geraEndereco(rota, token) {
   const baseURL = process.env.BASE_URL;
-  return `${baseURL}${rota}${id}`;
+  return `${baseURL}${rota}${token}`;
 }
 
 module.exports = {
@@ -21,7 +21,8 @@ module.exports = {
       await usuario.adicionaSenha(senha);
       await usuario.adiciona();
 
-      const endereco = geraEndereco('/usuario/verifica_email/',usuario.id);
+      const token = tokens.verificacaoEmail.cria(usuario.id);
+      const endereco = geraEndereco('/usuario/verifica_email/',token);
       const emailVerificacao = new EmailVerificacao(usuario, endereco);
       // Executa assíncrono, pois demora muito
       emailVerificacao.enviaEmail().catch(console.log);
@@ -60,6 +61,16 @@ module.exports = {
     try {
       const usuarios = await Usuario.lista();
       res.json(usuarios);
+    } catch (erro) {
+      res.status(500).json({ erro: erro.message });
+    }
+  },
+
+  async verificaEmail(req, res) {
+    try {
+      const usuario = await req.user;
+      await usuario.verificaEmail();
+      res.status(200).json();
     } catch (erro) {
       res.status(500).json({ erro: erro.message });
     }
